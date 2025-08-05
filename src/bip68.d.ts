dc24ec01284f4eb33b70800de04ce1ca1ac921b3
hirokuma <hiro9ma@gmail.com>
2025-08-05 18:58:24 +0900

    remove unused

--- src/bip68.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'bip68' {
-  const bip68: { encode: (opts: { blocks: number }) => number; decode: (n: number) => { blocks: number } };
-  export default bip68;
-}
-// declare module 'bip68';
